62bbd96e93f425ad6eed0bf3b4e18e5d9a68023a
Rachel Bonny <rachel.bonny@koahealth.com>
2021-09-17 10:12:52 +0200

    remove TO DO

--- lib/screen_lock_check.dart
+++ lib/screen_lock_check.dart
@@ -6,7 +6,6 @@ class ScreenLockCheck {
   static const MethodChannel _channel = const MethodChannel('screen_lock_check');
 
   static Future<bool> get isScreenLockEnabled async {
-    // TO DO: should this param to invokeMethod be camelCase?
     final bool isScreenLockEnabled = await _channel.invokeMethod('isScreenLockEnabled');
     return isScreenLockEnabled;
   }
