92673b833fc65f251e2e338b3672a261cac7d7df
Rachel Bonny <rachel.bonny@koahealth.com>
2021-09-22 10:06:32 +0200

    formatting

--- lib/screen_lock_check.dart
+++ lib/screen_lock_check.dart
@@ -3,10 +3,12 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class ScreenLockCheck {
-  static const MethodChannel _channel = const MethodChannel('screen_lock_check');
+  static const MethodChannel _channel =
+      const MethodChannel('screen_lock_check');
 
   static Future<bool> get isScreenLockEnabled async {
-    final bool isScreenLockEnabled = await _channel.invokeMethod('isScreenLockEnabled');
+    final bool isScreenLockEnabled =
+        await _channel.invokeMethod('isScreenLockEnabled');
     return isScreenLockEnabled;
   }
 }
